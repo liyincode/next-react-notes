e4bb94d1a75e918f4ba0dfbb770f942125b59567
liyin <liyincode@gmail.com>
2024-10-17 15:29:32 +0800

    feat: Sidebar note list add skeleton

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import Link from 'next/link'
-import {getAllNotes} from "@/lib/redis";
 import SidebarNoteList from "@/components/SidebarNoteList";
+import EditButton from "@/components/EditButton";
+import NoteListSkeleton from "@/components/NoteListSkeleton";
 
 export default async function Sidebar() {
-  const notes = await getAllNotes()
 
   return (
     <>
@@ -12,21 +12,26 @@ export default async function Sidebar() {
         <Link href={'/'} className="link--unstyled">
           <section className="sidebar-header">
             <img
-              className="logo"
-              src="/logo.svg"
-              width="22px"
-              height="20px"
-              alt=""
-              role="presentation"
+                className="logo"
+                src="/logo.svg"
+                width="22px"
+                height="20px"
+                alt=""
+                role="presentation"
             />
             <strong>React Notes</strong>
           </section>
         </Link>
         <section className="sidebar-menu" role="menubar">
-            {/* SideSearchField */}
+          {/* SideSearchField */}
+        </section>
+        <section className="sidebar-menu" role="menubar">
+          <EditButton noteId={null}>New</EditButton>
         </section>
         <nav>
-          <SidebarNoteList notes={notes} />
+          <Suspense fallback={<NoteListSkeleton/>}>
+          <SidebarNoteList/>
+          </Suspense>
         </nav>
       </section>
     </>
