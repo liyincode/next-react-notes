10c2a2559c0f056b9122e8dfe2e12fc5790023d5
liyin <liyincode@gmail.com>
2024-10-22 11:06:23 +0800

    feat: Replace SidebarNoteItem becoming a client component by passing it through props

--- components/SidebarNoteList.js
+++ components/SidebarNoteList.js
@@ -1,8 +1,10 @@
 import {getAllNotes} from "@/lib/redis";
 import SidebarNoteListFilter from "@/components/SidebarNoteListFilter";
+import SidebarNoteItemHeader from "@/components/SidebarNoteItemHeader";
 
 export default async function NoteList() {
     const notes = await getAllNotes()
+
     const noteList = Object.entries(notes)
 
     if (noteList.length === 0) {
@@ -13,5 +15,19 @@ export default async function NoteList() {
         )
     }
 
-    return (<SidebarNoteListFilter notes={noteList}/>)
+
+    return (
+        <SidebarNoteListFilter
+            notes={
+                noteList.map(([noteId, note]) => {
+                    const noteData = JSON.parse(note)
+                    return {
+                        noteId,
+                        note: noteData,
+                        header: <SidebarNoteItemHeader title={noteData.title} updateTime={noteData.updateTime}/>
+                    }
+                })
+            }
+        />
+    )
 }
--- components/SidebarNoteListFilter.js
+++ components/SidebarNoteListFilter.js
@@ -1,26 +1,35 @@
 'use client'
 
-import SidebarNoteItem from "@/components/SidebarNoteItem";
 import {useSearchParams} from "next/navigation";
+import SidebarNoteItemContent from "@/components/SidebarNoteItemContent";
 
 export default function SidebarNoteListFilter({notes}) {
     const searchParams = useSearchParams();
     const query = searchParams.get('q');
 
     if (query) {
-        notes = notes.filter(([noteId, note]) => {
-            const parsedNote = JSON.parse(note);
-            return parsedNote.title.includes(query);
+        notes = notes.filter(({note}) => {
+            return note.title.includes(query);
         });
     }
 
     return (
         <ul className="notes-list">
             {
-                notes.map(([noteId, note]) => {
+                notes.map(({noteId, note, header}) => {
                         return (
                             <li key={noteId}>
-                                <SidebarNoteItem noteId={noteId} note={JSON.parse(note)}/>
+                                <SidebarNoteItemContent
+                                    id={noteId}
+                                    title={note.title}
+                                    expandedChildren={
+                                        <p className="sidebar-note-excerpt">
+                                            {note.content?.substring(0, 20) || <i>(No content)</i>}
+                                        </p>
+                                    }
+                                >
+                                    {header}
+                                </SidebarNoteItemContent>
                             </li>
                         )
                     }
