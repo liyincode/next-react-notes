6650c12a7c5c120ab255f0cf4fd2d5c368389ce5
liyin <liyincode@gmail.com>
2024-10-23 14:55:05 +0800

    feat: add language switch

--- app/[lng]/layout.js
+++ app/[lng]/layout.js
@@ -1,6 +1,7 @@
 import './style.css'
 import Sidebar from '@/components/Sidebar'
 import {locales} from "@/config";
+import {Footer} from "@/components/Footer";
 
 export async function generateStaticParams() {
     return locales.map((lng) => ({lng}))
@@ -13,9 +14,10 @@ export default async function RootLayout({children, params: {lng}}) {
         <body>
         <div className="container">
             <div className="main">
-                <Sidebar/>
+                <Sidebar lng={lng}/>
                 <section className="col note-viewer">{children}</section>
             </div>
+            <Footer lng={lng}/>
         </div>
         </body>
         </html>
